15ab27fad0129d0848e99de856d3603d3638eb5a
shashiben <ben75930@gmail.com>
2020-06-14 03:48:09 +0530

    :tada: initial setup

---
+++ android/app/src/main/kotlin/com/example/face_mask_detection/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.face_mask_detection
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:face_mask_detection/ui/homepage.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: HomePage(),
+    );
+  }
+}
